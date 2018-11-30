f0d42e77a6d4f3cdecec72ec1b3c3712bccde725
TheaCsy <csy_orange@hotmail.com>
2018-11-30 14:08:40 +0800

    oop_introduction_1130

---
+++ oop-1130/01.py
@@ -0,0 +1,15 @@
+########
+
+class PythonStudent():
+    name = None
+    age = 18
+    course = "Python"
+
+    def Homework(self):
+        print("I am working")
+        return None
+
+xiaohong = PythonStudent()
+print(xiaohong.name)
+print(xiaohong.age)
+xiaohong.Homework()
