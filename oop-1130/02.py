b877f47b480f192dd610ef349c72c9eaa29cf2f6
TheaCsy <csy_orange@hotmail.com>
2018-12-06 11:33:13 +0800

    12-06

--- oop-1130/02.py
+++ oop-1130/02.py
@@ -1,11 +1,14 @@
 import math
 print(math)
-print(math.ceil(5.04))
-print(math.floor(5.04))
+print(math.ceil(5.04))  # 向上取整
+print(math.floor(5.04))  # 向下取整
 
 import keyword
 print(keyword.kwlist)
 
-print(round(5.1315))
-print(math.sqrt(9))
-print(math.pow(2,3))
+print(round(5.1315))  # 四舍五入
+print(math.sqrt(9))  # 平方根
+print(math.pow(2,3))  # 幂运算，返回浮点数
+print(2**3)  # 幂运算，返回整数
+
+
